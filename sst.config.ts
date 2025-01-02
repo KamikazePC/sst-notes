d6a2c7cc2ac8676efbd9d6dbe5ee7b54178870c6
PriestlyC <priestlychukwu@gmail.com>
2025-01-02 23:36:28 +0100

    Initial commit

--- sst.config.ts
+++ sst.config.ts
@@ -1,20 +1,16 @@
-/// <reference path="./.sst/platform/config.d.ts" />
+// <reference path="./.sst/platform/config.d.ts" />
 
 export default $config({
   app(input) {
     return {
-      name: "monorepo-template",
+      name: "sst-notes",
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
     };
   },
   async run() {
-    const storage = await import("./infra/storage");
+    await import("./infra/storage");
     await import("./infra/api");
-
-    return {
-      MyBucket: storage.bucket.name,
-    };
   },
 });
